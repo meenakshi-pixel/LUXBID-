6edbac611e4e3731e8ac729778dbc6564494aff2
Dhurga Meenakshi Yenduri <meenakshiyenduri@klh.edu.in>
2026-06-19 09:42:16 +0530

    Create HeroSection.jsx

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <div className="hero">
+
+      <div className="hero-content">
+
+        <h1>
+          Luxury Shopping <br />
+          Meets Smart Negotiation
+        </h1>
+
+        <p>
+          Discover premium products, negotiate prices,
+          and unlock exclusive deals in real time.
+        </p>
+
+        <button>
+          Explore Products
+        </button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default HeroSection;
